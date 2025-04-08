6751fbd3b903a4d00742ce4055aebc071f82d782
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-04-08 23:43:24 +0400

    Add custom-components library with initial setup and configuration

---
+++ projects/custom-components/src/lib/custom-components.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-custom-components',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      custom-components works!
+    </p>
+  `,
+  styles: ``
+})
+export class CustomComponentsComponent {
+
+}
---
+++ projects/custom-components/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of custom-components
+ */
+
+export * from './lib/custom-components.service';
+export * from './lib/custom-components.component';
